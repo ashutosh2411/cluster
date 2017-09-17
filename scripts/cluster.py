99965f377bd321e009b452463d4131c4c1e940b5
okabe <amishranjanranjan@gmail.com>
2017-09-17 20:51:50 +0530

    cluster using k means

--- scripts/cluster.py
+++ scripts/cluster.py
@@ -1,7 +1,8 @@
 """
 Program to cluster large number of documents
 """
-
+from os import listdir
+from os.path import isfile, join
 from collections import defaultdict
 import sklearn.datasets
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -9,16 +10,16 @@ from sklearn.cluster import KMeans
 import numpy as np
 import sklearn
 
-categories = [
-   "business" ,"sport", 'entertainment', 'tech', 'politics']
+data = '../datasets/bbc'
+categories = [f for f in listdir(data) if not isfile(join(data, f))]
+#print categories
+#categories = ["business" ,"sport", 'entertainment', 'tech', 'politics']
 
-dataset = sklearn.datasets.load_files(	'../datasets/bbcTest/',
-										description=None, categories=categories ,
+dataset = sklearn.datasets.load_files(	data + '/',	description=None, categories=categories ,
 										load_content=True, shuffle=True, 
 										encoding = 'utf-8',decode_error='ignore',
 										random_state=179863)
-
-true_k = 5 							#no of groups
+true_k = len(categories) 	#no of groups
 
 vectorizer = TfidfVectorizer(max_df=.5, max_features=310 ,min_df=10 ,
 										stop_words='english',use_idf=True)
@@ -26,8 +27,8 @@ vectorizer = TfidfVectorizer(max_df=.5, max_features=310 ,min_df=10 ,
 dat = vectorizer.fit_transform(dataset.data)
 dat = dat.toarray()
 
-X = dat[:125]
-Y = dat[125:]
+X = dat[:true_k**3]
+Y = dat[true_k**3:]
 
 km = KMeans(n_clusters=true_k)
 km.fit(X)
@@ -46,15 +47,15 @@ tmp = []
 
 for i in range(0,left_doc):
 	clust = []
-	for j in range (5):
+	for j in range (true_k):
 		x = km.cluster_centers_[j]
 		y = cur[i]
 		clust.append(sklearn.metrics.pairwise.euclidean_distances(x.reshape(1,-1) ,
 				 y.reshape(1,-1), Y_norm_squared=None, squared=False, X_norm_squared=None))
 	k =np.argmin(clust)
-	clusters[k].append(dataset.filenames[i+125])
+	clusters[k].append(dataset.filenames[i+true_k**3])
 	
-for i in range(5):
+for i in range(true_k):
 		for j in clusters[i]:
-			print(j)    
-		print()
+			print(j)
+		print ""
